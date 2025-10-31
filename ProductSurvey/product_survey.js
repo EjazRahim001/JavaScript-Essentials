b252e637b3a475ab5188b9f811905877636951e4
EjazRahim001 <ejaz.rahim125@gmail.com>
2025-11-01 00:58:12 +0500

    Product Survey form

--- ProductSurvey/product_survey.js
+++ ProductSurvey/product_survey.js
@@ -8,6 +8,7 @@ function submitFeedback() {
   const designation = document.getElementById('designation').value;
   const productType = document.getElementById('productType').value;
   const feedback = document.getElementById('feedbackText').value;
+  const userExp = document.getElementById('userExperience').value;
 
   document.getElementById('userName').innerHTML = username;
   document.getElementById('userAge').innerHTML = age;
@@ -16,7 +17,9 @@ function submitFeedback() {
   document.getElementById('userDesignation').innerHTML = designation;
   document.getElementById('userProductChoice').innerHTML = productType;
   document.getElementById('userFeedback').innerHTML = feedback;
+  document.getElementById('userExper').innerHTML = userExp;
   document.getElementById('userInfo').style.display = 'block';
+
 
   alert('Thank you for your valuable feedback');
 }
